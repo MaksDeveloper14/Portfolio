0dcf9901c163a48c6e216413fc8123138d431bdc
Маккси <Maksim-volkov1994@yandex.ru>
2020-11-19 12:14:18 +0300

    Без метатега viweport

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+function photoСhange() {
+	const myPhoto = document.getElementsByClassName('about-me__my-photo')[0];
+	// myPhoto.addEventListener('click', onClickMyPhoto);
+
+	let i = 0;
+
+	const arrSrc = [
+		'images/my-comp.jpg',
+		'images/моя фотография.jpg'
+	]
+
+	function onClickMyPhoto () {
+		myPhoto.src = arrSrc[i];
+		i++;
+		if(i > 1) {
+			i = 0;
+		}
+	} 
+
+	// меняем каждые 1 сек
+	setInterval(function() {
+		onClickMyPhoto();
+	}, 2000)
+}
+
+photoСhange();
+
+
