6f02dcaf33d3febcc5af6be89dddcf19942cfeaa
Маккси <Maksim-volkov1994@yandex.ru>
2020-12-16 00:32:56 +0300

    Анимация работ курсов Нетология

--- js/script.js
+++ js/script.js
@@ -1,4 +1,45 @@
 
+// 1.Показываем работы Нетология, 4 блока - при клике мышкой
+
+let netologyCourse = document.querySelector('.netology-course');
+let netologyCourse80 = document.querySelector('.netology-course-80');
+let netologyCourseHtml = document.querySelector('.netology-course-html');
+let netologyCourseJs = document.querySelector('.netology-course-js');
+let netologyCourseJsBr = document.querySelector('.netology-course-js-br');
+
+
+netologyCourse.addEventListener('click', function(e) {
+	// e.currentTarget.style.opacity = 0;
+	// e.currentTarget.style.display = 'none';
+	e.currentTarget.style.opacity = 0;
+
+	e.currentTarget.style.position = 'absolute';
+	e.currentTarget.style.visibility = 'hidden';
+
+	netologyCourse80.style.position = 'static';
+	netologyCourseHtml.style.position = 'static';
+	netologyCourseJs.style.position = 'static';
+	netologyCourseJsBr.style.position = 'static';
+
+	netologyCourse80.style.visibility = 'visible';
+	netologyCourseHtml.style.visibility = 'visible';
+	netologyCourseJs.style.visibility = 'visible';
+	netologyCourseJsBr.style.visibility = 'visible';
+
+	netologyCourse80.style.opacity = 1;
+	netologyCourseHtml.style.opacity = 1;
+	netologyCourseJs.style.opacity = netologyCourseJsBr.style.opacity = 1;1;
+	
+})
+
+
+
+
+
+
+
+
+
 function photoСhange() {
 	const myPhoto = document.getElementsByClassName('about-me__my-photo')[0];
 	// myPhoto.addEventListener('click', onClickMyPhoto);
